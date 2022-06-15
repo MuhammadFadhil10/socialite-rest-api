c188c583da20e5c7312e85fd5678f7b0b2542ab2
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-06-15 20:33:13 +0700

    remote mysql

--- database/db.js
+++ database/db.js
@@ -1,10 +1,10 @@
 import mysql2 from 'mysql2';
 
 const pool = mysql2.createPool({
-    user: 'root',
-    host: 'localhost',
-    password: 'Fadhil1010',
-    database: 'social_media'
+	user: '	sql6499852',
+	host: 'sql6.freemysqlhosting.net',
+	password: 'wSLMiByuyP',
+	database: 'sql6499852',
 });
 
 export default pool.promise();
