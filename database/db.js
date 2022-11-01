0ab12341e84d76060ad477783a24d419b6d2a78f
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-11-01 09:02:00 +0700

    update

--- database/db.js
+++ database/db.js
@@ -1,11 +1,11 @@
 import mysql2 from 'mysql2';
 
 const pool = mysql2.createPool({
-	user: 'admin',
-	host: 'mysql-80362-0.cloudclusters.net',
-	password: 'bDhzV75E',
-	database: 'socialite',
-	port: 19539,
+	user: 'jkyylcyemnjwbg',
+	host: 'ec2-44-206-89-185.compute-1.amazonaws.com',
+	password: '4c749be0c05b0a4b2d657e10c78b929c9ddac3ab5935ee96920e47fb89d796c1',
+	database: 'd9t6p0rju5qe1d',
+	port: 5432,
 });
 
 export default pool.promise();
